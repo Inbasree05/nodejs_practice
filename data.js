6a89583e5a3de9fae049790add2635dd21b34444
Inbasree05 <inbasreeselvaraj90@gmail.com>
2026-06-24 08:33:28 +0530

    Initial commit

---
+++ data.js
@@ -0,0 +1,40 @@
+const fs=require('fs');
+// if(!fs.existsSync('./docs')){
+//     fs.mkdir('./docs',(err)=>{
+//         if(err){
+//             console.log(err.message);
+//         }
+//         else{
+//             console.log("File Created Successfully");
+//         }
+//     }
+//     )
+// }
+// fs.writeFile('./docs/file.txt',"HI,sai",(err)=>{
+//     if(err){
+//         console.log(err.message);
+//     }
+//     else{
+//         console.log("File Created Successfully");
+//     }
+// })
+// if(fs.existsSync('./docs/file.txt')){
+//     fs.readFile('./docs/file.txt',(err,data)=>{
+//         if(err){
+//             console.log(err.message);
+//         }
+//         else{
+//             console.log(data.toString());
+//         }
+//     })
+// }
+if(fs.existsSync('./docs/file.txt')){
+    fs.appendFile('./docs/file.txt',"Inbasree here",(err)=>{
+        if(err){
+            console.log(err.message);
+        }
+        else{
+            console.log("File Updated Successfully");
+        }
+    })
+}
