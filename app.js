10db5aa9d55440ecb250e65d5e22a9cced2d2d48
Inbasree05 <inbasreeselvaraj90@gmail.com>
2026-06-27 14:38:34 +0530

    Middleware

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const exp=require('express');
 const app=exp();
 app.listen(3000);
+app.use((req,res,next)=>{
+    console.log("Hi,I am middleware");
+    console.log(req.path);
+    console.log(req.host);
+    next();
+})
 app.get('/',(req,res)=>{
     res.sendFile('./docs/index.html',{root:__dirname});
 })
