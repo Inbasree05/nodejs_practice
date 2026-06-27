23da65cf5d99c7d80a709fe767160cf1429e314a
Inbasree05 <inbasreeselvaraj90@gmail.com>
2026-06-27 07:00:55 +0530

    urls

--- server2.js
+++ server2.js
@@ -2,13 +2,28 @@ const http=require('http');
 const fs=require('fs');
 const server=http.createServer((req,res)=>{
     res.setHeader('Content-Type','text/html');
-    fs.readFile('./docs/index.html',(err,data)=>{
+    
+    let path='./docs/';
+    if(req.url=='/home'){
+   res.writeHead(301,{Location:'/'});
+    return res.end();
+    }
+else if(req.url=='/'){
+    path+='index.html';
+}
+    else if(req.url=='/about'){
+        path+='abot.html';
+        res.statusCode=200;
+    }
+    fs.readFile(path,(err,data)=>{
         if(err){
             res.writeHead(404);
-            res.write("Error: File Not Found");
+            return res.end("Error: File Not Found");
         }
         else{
-            res.end(data);
+        res.writeHead(200);
+        res.end(data);
+
         }
     })
 })
